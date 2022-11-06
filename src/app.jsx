82ebe90a226c7672b61a17d468803f290d46a53c
Seolyu <leeseolyu@gmail.com>
2022-11-07 01:25:17 +0900

    router

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,41 @@
 import logo from './logo.png';
 import './app.css';
 import './assets/styles/base/reset.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Dday from './pages/Dday';
+import Timetable from './pages/Timetable';
+import Todolist from './pages/Todolist';
+import Root from './pages/Root';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Root />,
+    errorElement: <p>그런 경로 없습니다</p>,
+    children: [
+      {
+        index: true, element: <Home />
+      },
+      {
+        path: 'd-day',
+        element: <Dday />
+      },
+      {
+        path: 'timetable',
+        element: <Timetable />
+      },
+      {
+        path: 'todo-list',
+        element: <Todolist />
+      }
+    ]
+  }
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/pages/Dday.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Dday() {
+    return <div>디데이</div>;
+}
